55628a18f760ce7019e52506f51fd074ef8d5b12
James-Welsh <j.welsh97@protonmail.com>
2020-11-27 18:21:13 +0000

    started code

---
+++ sourceCode/LibraryMember.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class LibraryMember {
+
+    private String memberId;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private ArrayList<Book> books;
+    private ArrayList<String> messages;
+
+    public LibraryMember() {
+
+    }
+}
